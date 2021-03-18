2ad2a43c6f89116b23d142b46f1cd28e3941d0fa
P-Hsgw <contact@p-hsgw.com>
2021-03-18 14:53:00 +0900

    Add grid coloring on hoover, random color selector, proper grid spacing

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const container = document.querySelector(".grid-container")
+const box = document.querySelectorAll("#square")
 
 
 function gridCreator() {
 
   let userChoice = prompt("Wpisz liczbę")
   userChoiceInt = parseInt(userChoice)
- /* userChoiceInt = typeof message !== 'undefined' ? userChoiceInt : 16;*/
-  if (userChoiceInt > 50) {
+  if (userChoiceInt > 100) {
     alert ("Error - number too big");
   } else {
     console.log(userChoiceInt);
     let i;
-    userChoiceInt = Math.pow(userChoiceInt, 2)
-    for (i=0; i < userChoiceInt; i++) {
+    userChoiceIntSquare = Math.pow(userChoiceInt, 2)
+    for (i=0; i < userChoiceIntSquare; i++) {
       const div = document.createElement('div');
-      div.textContent=`Berro${i}`;
-      container.appendChild(div)
-      console.log(userChoiceInt)
-      
-  }
-  
+      div.setAttribute('id', 'square'); 
+      container.appendChild(div);
+      div.addEventListener('mouseover', function (e) {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.background = `#${randomColor}`;
+      });
+    }
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${userChoiceInt}, 1fr)`;
+
   }
 }
 
-gridCreator()
+gridCreator();
