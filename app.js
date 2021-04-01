305d0c67b9445d77dba4da71a3dd1241383dcbbc
P-Hsgw <contact@p-hsgw.com>
2021-04-01 16:53:15 +0900

    Run linting and Prettier on the code, to make it more readable

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const container = document.querySelector(".grid-container")
-const box = document.querySelectorAll("#square")
-const size = document.getElementById("size")
-const erase = document.getElementById("erase")
+const container = document.querySelector(".grid-container");
+const size = document.getElementById("size");
+const erase = document.getElementById("erase");
 
 // Define default size of a grid - 16x16
-const defaultState = 16; 
+const defaultState = 16;
 
 // Add a typewriter effect in a title
-let typeWriterIterator = 0
+let typeWriterIterator = 0;
 function typeWriter() {
-  let txt = 'Etch-a-sketch';
+  let txt = "Etch-a-sketch";
   let speed = 75;
   if (typeWriterIterator < txt.length) {
-    document.getElementById("typing").innerHTML += txt.charAt(typeWriterIterator);
+    document.getElementById("typing").innerHTML += txt.charAt(
+      typeWriterIterator
+    );
     typeWriterIterator++;
     setTimeout(typeWriter, speed);
   }
@@ -20,23 +21,25 @@ function typeWriter() {
 
 // Create a grid with chosen state/size and implement basic functionality of the game, while generating random colors
 function gridCreator(state) {
-  let stateInt = parseInt(state)
-  let ustateIntSquare = Math.pow(stateInt, 2)
+  let stateInt = parseInt(state);
+  let ustateIntSquare = Math.pow(stateInt, 2);
   let i;
-  for (i=0; i < ustateIntSquare; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'square'); 
+  for (i = 0; i < ustateIntSquare; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "square");
     container.appendChild(div);
-    div.addEventListener('mouseover', function (e) {
-      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    div.addEventListener("mouseover", function (e) {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
       e.target.style.background = `#${randomColor}`;
     });
   }
-  document.getElementById("container").style.gridTemplateColumns = `repeat(${stateInt}, 1fr)`;
+  document.getElementById(
+    "container"
+  ).style.gridTemplateColumns = `repeat(${stateInt}, 1fr)`;
 }
 
 // Clear the grid from colors
-function clearGrid () {
+function clearGrid() {
   const gridArray = Array.from(container.childNodes);
   gridArray.forEach((element) => {
     container.removeChild(element);
@@ -45,29 +48,29 @@ function clearGrid () {
 
 // Let user choose the grid size and catches errors
 function gridPrompt() {
-  let choice = prompt("Enter number from 1 to 64")
+  let choice = prompt("Enter number from 1 to 64");
   if (isNaN(choice) || choice === "" || choice === null) {
-    return 16
+    return 16;
   } else if (choice > 64) {
-    alert ("Error - number too big");
-    return 16
+    alert("Error - number too big");
+    return 16;
   } else {
     return choice;
   }
 }
 
 // Listen to user click and let them chose the grid size with gridPrompt, and reset the grid with clearGrid
-size.addEventListener(`click`, e => {
-  clearGrid()
-  let newState = gridPrompt()
-  gridCreator(newState)
-})
+size.addEventListener(`click`, () => {
+  clearGrid();
+  let newState = gridPrompt();
+  gridCreator(newState);
+});
 
 // Let user reset the grid with clearGrid
-erase.addEventListener(`click`,() => {
-  clearGrid()
-  gridCreator(defaultState)
-})
+erase.addEventListener(`click`, () => {
+  clearGrid();
+  gridCreator(defaultState);
+});
 
 // Initiates the grid with defaultState
 gridCreator(defaultState);
